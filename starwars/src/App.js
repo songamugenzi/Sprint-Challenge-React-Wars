72e020bfca0929480032701530758e31de494f11
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-05-15 13:10:33 -0400

    Renders characters on page, styling in progress.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 import './App.css';
+import styled from 'styled-components'
+import Character from "./components/Character"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [data, setData] = useState({})
+  const [data, setData] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -13,7 +15,8 @@ const App = () => {
   useEffect(() => {
     axios.get('https://rickandmortyapi.com/api/character/')
     .then((res) => {
-      console.log(res.data)
+      setData(res.data.results)
+      // console.log(res.data.results)
     })
     .catch((err) =>{
       console.log('Error with API get request', err)
@@ -22,9 +25,27 @@ const App = () => {
   
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">The Rick and Morty Show</h1>
+      <CharacterCast>{
+        data.map((character, index) => {
+          return (
+            <Character key={index} character={character}/>
+          )
+        })
+      }</CharacterCast>
     </div>
   );
 }
+
+const CharacterCast = styled.ul`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-content:center;
+  /* background: black; */
+  width: 80%;
+  margin: 2% auto;
+  list-style-type: none;
+`
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,51 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+
+function Character(props) {
+    const { image, name, species, status } = props.character
+    return (
+        <CharacterBio>
+            <div>
+                <img src={image} alt={name} />
+            </div>
+            <div className="textBio">
+                <h3>{name}</h3>
+                <p>{` `}
+                    <span>
+                        Species{` `}
+                        <i className="fas fa-dna"></i>{` `}:{` `}
+                    </span>{species}
+                </p>
+                <p>{`Status: ${status}`}</p>
+            </div>
+        </CharacterBio>
+    )
+}
+
+const CharacterBio = styled.li`
+    color: #333;
+    background: aliceblue;
+    text-align: left;
+    margin-top: 5%;
+    border-radius: 12px 12px 0 0;
+
+    img {
+        border-radius: 12px 12px 0 0;
+        width: 100%;
+    }
+
+    .textBio {
+        padding: 0 10px;
+    }
+
+    h3 {
+        color: lightcoral;
+    }
+
+    span {
+
+    }
+`
+
+export default Character;
